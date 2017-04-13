1dc5bee69b2b9cca2106532d644ed65e110776d0
andygimma <andy.gimma@kiron.ngo>
2017-04-12 20:54:20 -0400

    Setup Redux and Saga

---
+++ src/store.js
@@ -0,0 +1,24 @@
+import {
+  createStore,
+  applyMiddleware,
+  compose
+} from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from './rootReducer';
+import rootSaga from './rootSaga';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const sagaMiddleware = createSagaMiddleware();
+
+const configureStore = () => {
+  const store = {
+    ...createStore(rootReducer, composeEnhancers(
+      applyMiddleware(sagaMiddleware)))
+  };
+
+  sagaMiddleware.run(rootSaga);
+
+  return store;
+};
+
+export default configureStore;
